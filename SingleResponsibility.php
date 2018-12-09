032ab8541be1d4f79c36a854fce4105eb5fe67fe
Yusef Mohamadi <yuseferi@gmail.com>
2018-12-09 14:33:39 +0330

    Single Responsibility Sample

---
+++ SingleResponsibility.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * An example of a Class that violates the Single Responsibility Principle.
+ * Creation of customer and order mixed up.
+ */
+
+
+// Bad Example
+class CustomerOrder {
+     
+    public function createCustomer(Request $request) {
+        // Create customer
+    }
+     
+    public function submitOrder(Request $request) {
+        // Submit Orders for processing
+    }
+}
+
+
+
+// Good Example
+class Customer {
+     
+    public function createCustomer(Request $request) {
+        // Create customer
+    }
+     
+}
+
+class Order {
+     
+    public function submitOrder(Request $request) {
+        // Submit Orders
+    }
+     
+}
+
+?>
